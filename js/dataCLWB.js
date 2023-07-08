70bf79a9fcb6cd462ef6508fb352764982da8260
Valerii Bryksin <103672482+W1ndDancer@users.noreply.github.com>
2023-07-08 12:29:10 +0200

    hw11 is done

---
+++ js/dataCLWB.js
@@ -0,0 +1,44 @@
+const dataInfo = `
+[
+    {
+        'img': 'img/products/1.svg',
+        'name': 'ELLERY X M'O CAPSULE',
+        'description': 'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        'price': '52'
+    },
+
+    {
+        'img': 'img/products/2.svg',
+        'name': 'ELLERY X M'O CAPSULE',
+        'description': 'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        'price': '52'
+    },
+
+    {
+        'img': 'img/products/3.svg',
+        'name': 'ELLERY X M'O CAPSULE',
+        'description': 'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        'price': '52'
+    },
+
+    {
+        'img': 'img/products/4.svg',
+        'name': 'ELLERY X M'O CAPSULE',
+        'description': 'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        'price': '52'
+    },
+
+    {
+        'img': 'img/products/5.svg',
+        'name': 'ELLERY X M'O CAPSULE',
+        'description': 'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        'price': '52'
+    },
+    
+    {
+        'img': 'img/products/6.svg',
+        'name': 'ELLERY X M'O CAPSULE',
+        'description': 'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        'price': '52'
+    }
+]`
