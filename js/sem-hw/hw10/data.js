1c7cd55e7a7f59670fdd651aa7152d42af7f7754
Valerii Bryksin <103672482+W1ndDancer@users.noreply.github.com>
2023-07-08 11:17:49 +0200

    hw10 is done

---
+++ js/sem-hw/hw10/data.js
@@ -0,0 +1,32 @@
+const dataInfo = `
+[
+    {
+        "activity": "Plan a trip to another country",
+        "type": "recreational",
+        "participants": 1,
+        "price": 0,
+        "link": "",
+        "key": "5554727",
+        "accessibility": 0
+      },
+
+      {
+        "activity": "Learn origami",
+        "type": "education",
+        "participants": 1,
+        "price": 0.2,
+        "link": "",
+        "key": "8394738",
+        "accessibility": 0.3
+      },
+
+      {
+        "activity": "Configure two-factor authentication on your accounts",
+        "type": "busywork",
+        "participants": 1,
+        "price": 0,
+        "link": "https://en.wikipedia.org/wiki/Multi-factor_authentication",
+        "key": "1572120",
+        "accessibility": 0
+      }
+]`
